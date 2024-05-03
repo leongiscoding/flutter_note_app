7db8f4b7d6727a0f501989a4028a79a36d6663ca
Leong2Bob <leongcoding1122@gmail.com>
2024-05-03 15:42:36 +0800

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_note_app/pages/note_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
      debugShowCheckedModeBanner: false,
-      home: const MyApp(),
+      home: const NotePage(),
     );
   }
 }
