7946ea93c86f6251ec1dbea69bf8c4b2efc99b6e
Leong2Bob <leongcoding1122@gmail.com>
2024-05-04 18:46:50 +0800

    Done implement navigation, added description too

--- lib/components/drawer.dart
+++ lib/components/drawer.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_note_app/components/drawer_tile.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class MyDrawer extends StatelessWidget {
   const MyDrawer({super.key});
@@ -12,6 +13,13 @@ class MyDrawer extends StatelessWidget {
         children: [
           const DrawerHeader(
               child: Icon(Icons.edit,size: 48,),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 10.0,bottom: 10.0),
+            child: Text(
+              "Easy Life, Easy Go...",
+              style: GoogleFonts.dmSerifText(color: Theme.of(context).colorScheme.inversePrimary,fontSize: 20,)
+            ),
           ),
 
           //notes tile
@@ -26,6 +34,7 @@ class MyDrawer extends StatelessWidget {
             title: "Settings",
             leading: const Icon(Icons.settings),
             onTap: (){
+              Navigator.pop(context);
               Navigator.pushNamed(context, "/settingsPage");
             },
           ),
